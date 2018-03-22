67e63306d804eee5fa44ea798986b3caaf7c8de8
Dan Rees <dan.rees@finkit.com>
2018-03-22 08:25:28 +0000

    Fixed test script.

--- ci/scripts/test_jenkins.sh
+++ ci/scripts/test_jenkins.sh
@@ -2,7 +2,7 @@
 
 export TERM=dumb &&\
 cp --archive godog_test/src ${GOPATH}/src &&\
-cd ${GOPATH}/godog_test &&\
+cd ${GOPATH}/src/godog_test &&\
 go get github.com/DATA-DOG/godog/cmd/godog
 mkdir bin/tests
 godog --format=cucumber --output bin/tests/jenkins --strict $(pwd)/src/buildstack/features/jenkins.feature
