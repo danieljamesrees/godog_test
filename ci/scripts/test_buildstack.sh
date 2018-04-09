b15f38c7faa65d179a355daf1311f7b2b704ec75
Developer Environment <dan.rees@finkit.com>
2018-04-09 11:38:13 +0000

    Rename job and fix SSH directory.

--- ci/scripts/test_buildstack.sh
+++ ci/scripts/test_buildstack.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 export TERM=dumb &&\
+mkdir ~/.ssh &&\
+chmod u=rwx,go= ~/.ssh &&\
+echo "${JUMPBOX_PRIVATE_KEY}" > ~/.ssh/jumpbox.key &&\
+chmod u=rw,go= ~/.ssh/jumpbox.key &&\
 cp -r godog-test/gosrc/* ${GOPATH} &&\
 cd godog-test &&\
-echo "${JUMPBOX_PRIVATE_KEY}" > ~/.ssh/jumpbox.key
-chmod u=rw,go= ~/.ssh/jumpbox.key
 ./build_and_run.sh "${JUMPBOX_ADDRESS}" ~/.ssh/jumpbox.key "${CREDHUB_USERNAME}" "${CREDHUB_PASSWORD}" "${CREDHUB_PROXY_PORT}"
 rm ~/.ssh/jumpbox.key
