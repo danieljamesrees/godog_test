119b9047f69cd6ea1a8cfbdcd3072ece2572054d
Developer Environment <dan.rees@finkit.com>
2018-04-09 11:35:15 +0000

    Fix key permissions.

--- ci/scripts/test_buildstack.sh
+++ ci/scripts/test_buildstack.sh
@@ -3,6 +3,7 @@
 export TERM=dumb &&\
 cp -r godog-test/gosrc/* ${GOPATH} &&\
 cd godog-test &&\
-echo "${JUMPBOX_PRIVATE_KEY}" > /tmp/jumpbox.key
-./build_and_run.sh "${JUMPBOX_ADDRESS}" /tmp/jumpbox.key "${CREDHUB_USERNAME}" "${CREDHUB_PASSWORD}" "${CREDHUB_PROXY_PORT}"
-rm > /tmp/jumpbox.key
+echo "${JUMPBOX_PRIVATE_KEY}" > ~/.ssh/jumpbox.key
+chmod u=rw,go= ~/.ssh/jumpbox.key
+./build_and_run.sh "${JUMPBOX_ADDRESS}" ~/.ssh/jumpbox.key "${CREDHUB_USERNAME}" "${CREDHUB_PASSWORD}" "${CREDHUB_PROXY_PORT}"
+rm ~/.ssh/jumpbox.key
