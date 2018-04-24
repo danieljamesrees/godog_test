221fbc8420c59daa49f78b4239c16361d97624c2
Developer Environment <dan.rees@finkit.com>
2018-04-24 15:54:25 +0000

    Install bbl 6

--- ci/scripts/test_buildstack.sh
+++ ci/scripts/test_buildstack.sh
@@ -5,6 +5,9 @@ mkdir ~/.ssh &&\
 chmod u=rwx,go= ~/.ssh &&\
 echo "${JUMPBOX_PRIVATE_KEY}" > ~/.ssh/jumpbox.key &&\
 chmod u=rw,go= ~/.ssh/jumpbox.key &&\
+curl --location --output bbl https://github.com/cloudfoundry/bosh-bootloader/releases/download/v6.6.5/bbl-v6.6.5_linux_x86-64 &&\
+chmod u+x bbl &&\
+sudo mv bbl /usr/local/bin/ &&\
 cd buildstack-bbl-state/buildstack-bbl-state &&\
 eval "$(bbl print-env)" &&\
 cd .. &&\
