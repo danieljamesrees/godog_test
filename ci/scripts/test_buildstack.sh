8b9a262eaca658d6438b75b4d30c486e5a5d1d46
Developer Environment <dan.rees@finkit.com>
2018-04-24 11:51:04 +0000

    Set up for Concourse.

--- ci/scripts/test_buildstack.sh
+++ ci/scripts/test_buildstack.sh
@@ -5,7 +5,10 @@ mkdir ~/.ssh &&\
 chmod u=rwx,go= ~/.ssh &&\
 echo "${JUMPBOX_PRIVATE_KEY}" > ~/.ssh/jumpbox.key &&\
 chmod u=rw,go= ~/.ssh/jumpbox.key &&\
+cd buildstack-bbl-state/buildstack-bbl-state &&\
+eval "$(bbl print-env)" &&\
+cd .. &&\
 cp -r godog-test/gosrc/* ${GOPATH} &&\
 cd godog-test &&\
-./build_and_run.sh "${JUMPBOX_ADDRESS}" ~/.ssh/jumpbox.key "${CREDHUB_USERNAME}" "${CREDHUB_PASSWORD}" "${CREDHUB_PROXY_PORT}"
+./build_and_run.sh "${JUMPBOX_ADDRESS}" "${CREDHUB_PROXY_PORT}"
 rm ~/.ssh/jumpbox.key
